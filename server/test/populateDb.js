d07841556d6314b0142df9dec80fadfe510f4a3d
xdaruis <rozdaruis@gmail.com>
2025-02-17 23:12:05 +0200

    chore: add node prefix to import

--- server/test/populateDb.js
+++ server/test/populateDb.js
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client';
-import { execSync } from 'child_process';
+import { execSync } from 'node:child_process';
 
 const mainAppURL = process.env.DATABASE_URL;
 process.env.DATABASE_URL = `${mainAppURL}_test`;
